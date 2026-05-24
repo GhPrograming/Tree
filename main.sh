7ed6ac6ff1ed5d9c1401e8c34c309364d2e31a4b
GH cyber <89rx77@gmail.com>
2026-05-24 21:53:42 +0300

    Add files via upload

---
+++ main.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo -e "\e[32m"
+echo "         /\         "
+echo "        /  \        "
+echo "       /____\       "
+echo "       /\  /\       "
+echo "      /  \/  \      "
+echo "     /________\     "
+echo "     /\  /\  /\     "
+echo "    /  \/  \/  \    "
+echo "   /____________\   "
+echo -e "\e[33m"
+echo "        ||||        "
